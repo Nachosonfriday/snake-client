6d4779bd53cba6490c3d53cc3a4483c73bb510e9
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-16 15:46:21 -0700

    moved connect function to own file, and added name to appear on connection for snake

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server')
+  })
+
+  conn.on('connect', () => {
+    conn.write('Name: MAZ');
+  });
+
+  conn.on("data", (data) => {
+    console.log('this is the data', data);
+  });
+
+  
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
