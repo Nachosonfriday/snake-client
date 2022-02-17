8d44c4aad50b680517ec66d625fcd9699f8c6370
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-16 19:15:43 -0700

    assigned keystrokes to directions

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+  const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,16 +9,34 @@ const setupInput = function () {
   stdin.on("data", (key) => {
     (handleUserInput(key));
   }) 
+  const handleUserInput = function (key) {
+    if (key === 'w') {
+      conn.write('Move: up');
+    }
+  
+    if (key === 'a') {
+      conn.write('Move: left');
+    }
+  
+    if (key === 's') {
+      conn.write('Move: down');
+    }
+  
+    if (key === 'd') {
+      conn.write('Move: right');
+    }
+  
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+
+
   return stdin;
 };
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 module.exports = {
   setupInput,
-  handleUserInput
 };
